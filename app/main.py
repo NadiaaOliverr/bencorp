df470d2db86ff62890b22320d48f3fefd258874c
Nádia Oliveira <nasilvah21@gmail.com>
2025-06-21 05:56:26 -0300

    chore(main): adicionar de forma mais completa a descrição da API

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.routes import vendas
 
-app = FastAPI()
+app = FastAPI(
+    title="Case Bencorp",
+    version="1.0.0",
+    description=(
+        "API desenvolvida para o case da Bencorp. "
+        "Permite realizar operações de CRUD de vendas com validações de dados, "
+        "integração com banco de dados relacional (PostgreSQL) e endpoints para ingestão e processamento "
+        "de dados (ETL). Tem suporte a paginação e filtros."
+    )
+)
+
 app.include_router(vendas.router)
