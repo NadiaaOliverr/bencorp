c6ddaf96dc56f82b8e236fa12727a4bfe65fe31a
Nádia Oliveira <nasilvah21@gmail.com>
2025-06-22 09:56:46 -0300

    feat(etl): adicionar endpoints para exportar dados e relatórios

--- app/routes/etl_routes.py
+++ app/routes/etl_routes.py
@@ -1,12 +1,39 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException, Depends, status
+from fastapi import APIRouter, UploadFile, File, HTTPException, Query, Depends, status
 from sqlalchemy.orm import Session
 from app.models import Venda
 from app.database import get_db
-from app.etl import processar_csv
+from app.etl import processar_csv, gerar_relatorio_mensal, gerar_top_vendedores_mensal, exportar_dados
 from app.crud import criar_vendas_em_lote
-
+from typing import Optional
 
 router = APIRouter(prefix="/etl", tags=["ETL"])
+
+@router.get("/exportar-dados", status_code=200)
+def exportar_dados_csv_json(formato:str = Query(...), categoria: Optional[str] = Query(None), vendedor:Optional[str] = Query(None), db:Session = Depends(get_db)):
+    """ 
+    Exporta os dados de vendas como CSV ou JSON na resposta. Permite filtros por categoria e vendedor.
+    """
+    return exportar_dados(db, formato, categoria, vendedor)
+
+@router.get("/relatorio-mensal", status_code=status.HTTP_200_OK)
+def relatorio_mensal(mes: str = Query(..., description="Formato YYYY-MM"), db: Session = Depends(get_db)):
+    """
+    Gera um relatório mensal de vendas retornando o agregado do mês, incluindo o maior vendedor.
+    """
+
+    relatorio = gerar_relatorio_mensal(db, mes)
+
+    return relatorio
+
+@router.get("/top-vendedores", status_code=status.HTTP_200_OK)
+def top_vendedores(mes: str, top: int = Query(3), db: Session = Depends(get_db)):
+    """
+    Retorna o total de vendas do TOP N vendedores do mês especificado e detalha as vendas feitas por categoria.
+    """
+    relatorio = gerar_top_vendedores_mensal(db, mes, top)
+    
+    return relatorio
+
 
 @router.post("/importar-csv", status_code=status.HTTP_200_OK)
 def importar_csv(file: UploadFile = File(...), db: Session = Depends(get_db)):
